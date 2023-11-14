33925a8515d2fb359ac5f2f17707b370bd76cc0e
bloodraven01 <122503351+bloodraven01@users.noreply.github.com>
2023-11-14 13:01:41 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,17 +40,28 @@ int main(int argc, char * argv[]) {
     SDL_Event e;
     SDL_Window * window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_Texture * bg = loadTexture("pexels-ahmad-18536470.jpg", renderer);
+    SDL_Texture * bg = loadTexture("assets\\bg\\pexels-ömer-derinyar-17718121.jpg", renderer);
     bool running = true;
     double move_speed = 10;
 
     //SDL_Rect r1{20, 700, 40, 40};
-    Player main_player(0, 40);
+
+    SDL_Texture * player_f1 = loadTexture("assets\\player\\walk_right_1.png", renderer);
+    SDL_Texture * player_f2 = loadTexture("assets\\player\\walk_right_2.png", renderer);
+    SDL_Texture * player_f3 = loadTexture("assets\\player\\walk_left_1.png", renderer);
+    SDL_Texture * player_f4 = loadTexture("assets\\player\\walk_left_2.png", renderer);
+
+    SDL_Texture * current_texture = player_f1;
+
+    Player main_player(0, 40, 2);
+
     //SDL_RenderDrawRect(renderer, &r1);
 
 
 
     while (running) {
+
+
 
         while (SDL_PollEvent(&e)) {
 
@@ -95,9 +106,50 @@ int main(int argc, char * argv[]) {
         //SDL_SetRenderDrawColor(renderer, 255, 255,  255, 255);
         
         SDL_RenderCopy(renderer, bg, NULL, NULL);
-        SDL_RenderFillRect(renderer, main_player.get_sprite());
+
+        if (main_player.moving_forward) {
+            
+            if (current_texture == player_f1) {
+        
+                SDL_RenderCopy(renderer, current_texture, NULL, main_player.get_sprite());
+                current_texture = player_f2;
+                
+            }
+
+            else {
+                
+            SDL_RenderCopy(renderer, current_texture, NULL, main_player.get_sprite());
+            current_texture = player_f1;
+            
+            }
+        }
+
+        else {
+
+            if (current_texture == player_f3) {
+        
+                SDL_RenderCopy(renderer, current_texture, NULL, main_player.get_sprite());
+                current_texture = player_f4;
+                
+            }
+
+            else {
+                
+            SDL_RenderCopy(renderer, current_texture, NULL, main_player.get_sprite());
+            current_texture = player_f3;
+
+
+
+
+        }
+        }
+
+        
+
+        //SDL_RenderFillRect(renderer, main_player.get_sprite());
+        SDL_SetRenderTarget(renderer, player_f1);
         SDL_RenderPresent(renderer);
-        SDL_Delay(10);
+        SDL_Delay(200);
 
     }
 
